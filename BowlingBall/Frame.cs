a7d8aebb2da7ecfff1d265aec20fba28f3ad7afb
erdeepalisinghavi <65441805+erdeepalisinghavi@users.noreply.github.com>
2020-05-16 13:35:51 +0530

    Add files via upload

---
+++ BowlingBall/Frame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Frame
+    {
+        public int firstRollPinDownValue;
+        public int secondRollPinDownValue;
+        public int extraRollPinDownValue;
+        public int frameScore;
+        public bool IsSpare;
+        public bool IsStrike;
+        public bool IsLastFrame;
+
+        public Frame(int firstRollPinDownCount)
+        {
+            firstRollPinDownValue = firstRollPinDownCount;
+            frameScore = firstRollPinDownCount;
+            if (firstRollPinDownValue == 10)
+                IsStrike = true;
+        }
+
+        public Frame(int firstRollPinDownCount,int secondRollPinDownCount)
+        {
+           firstRollPinDownValue = firstRollPinDownCount;
+           secondRollPinDownValue = secondRollPinDownCount;
+            frameScore = firstRollPinDownValue + secondRollPinDownValue;
+            if (frameScore == 10)
+                IsSpare = true;
+        }
+
+        public Frame(int firstRollPinDownCount, int secondRollPinDownCount, int extraRollPinDownCount)
+        {
+            firstRollPinDownValue = firstRollPinDownCount;
+            secondRollPinDownValue = secondRollPinDownCount;
+            extraRollPinDownValue = extraRollPinDownCount;
+            frameScore = firstRollPinDownValue + secondRollPinDownValue + extraRollPinDownValue;
+            IsLastFrame = true;
+        }
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        private readonly List<Frame> _frames;
+
+        public Game()
+        {
+            _frames = new List<Frame>();
+        }
+
+        public void CreateFrame(int firstRollPinDownCount)
+        {
+            _frames.Add(new Frame(firstRollPinDownCount));
+        }
+
+        public void CreateFrame(int firstRollPinDownCount, int secondRollPinDownCount)
+        {
+            _frames.Add(new Frame(firstRollPinDownCount, secondRollPinDownCount));
+        }
+
+        public void CreateFrame(int firstRollPinDownCount, int secondRollPinDownCount, int extraRollPinDownCount)
+        {
+            _frames.Add(new Frame(firstRollPinDownCount, secondRollPinDownCount, extraRollPinDownCount));
+        }
+
+        public int GetScore()
+        {
+            RecalculateFrameScore();
+            var frameScores = _frames.Select(i => i.frameScore).ToList();
+            return (frameScores.Sum());
+        }
+
+        private void RecalculateFrameScore()
+        {
+            for (int i = 0; i < _frames.Count-1; i++)
+            {
+                if (_frames[i].IsStrike)
+                {
+                    if (_frames[i + 1].IsLastFrame)
+                        _frames[i].frameScore = _frames[i].frameScore + _frames[i + 1].firstRollPinDownValue + _frames[i + 1].secondRollPinDownValue;
+                    else if (_frames[i + 1].IsStrike)
+                        _frames[i].frameScore = _frames[i].frameScore + _frames[i + 1].frameScore + _frames[i + 2].firstRollPinDownValue;
+                    else
+                        _frames[i].frameScore = _frames[i].frameScore + _frames[i + 1].frameScore;
+                }
+                else if (_frames[i].IsSpare)
+                {
+                    _frames[i].frameScore = _frames[i].frameScore + _frames[i + 1].firstRollPinDownValue;
+                }
+            }
+        }
+    }
+}
